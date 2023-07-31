8ea2daade313d4bc4907c408cf9db63f57209823
Khashayar Nowruzi <khrnowruzi@gmail.com>
2023-08-01 01:09:40 +0330

    Setting Services

---
+++ src/code/UsersManagement.WebAPI/Extensions/ApplicationServicesExtensions.cs
@@ -0,0 +1,19 @@
+﻿using UsersManagement.Application.Services;
+using UsersManagement.Core.Interfaces.IRepository;
+using UsersManagement.Core.Interfaces;
+using UsersManagement.Infrastracture.Data;
+using UsersManagement.Infrastracture.Repositories;
+
+namespace UsersManagement.WebAPI.Extensions
+{
+    public static class ApplicationServicesExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddScoped<IUserService, UserService>();
+            return services;
+        }
+    }
+}
