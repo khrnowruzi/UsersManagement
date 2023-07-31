809c9a3d242ceba8f884d8c3fb8be77a5b0450fb
Khashayar Nowruzi <khrnowruzi@gmail.com>
2023-07-31 21:25:07 +0330

    Setup ApplicationDbContext and adding to container.

---
+++ src/code/UsersManagement.Infrastracture/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UsersManagement.Core.Entities;
+
+namespace UsersManagement.Infrastracture.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
