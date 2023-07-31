7e24ee45711e4b0ad89714e091e20d2bcc722501
Khashayar Nowruzi <khrnowruzi@gmail.com>
2023-07-31 21:05:09 +0330

    Adding User entity

---
+++ src/code/UsersManagement.Core/Entities/BaseEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace UsersManagement.Core.Entities
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ src/code/UsersManagement.Core/Entities/User.cs
@@ -0,0 +1,9 @@
+﻿namespace UsersManagement.Core.Entities
+{
+    public class User : BaseEntity
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime Birthday { get; set; }
+    }
+}
