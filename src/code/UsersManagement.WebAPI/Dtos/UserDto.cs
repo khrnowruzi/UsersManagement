a35c228d66bcfc8758ff8bb0d96a46dfe9ecdb4f
Khashayar Nowruzi <khrnowruzi@gmail.com>
2023-07-31 21:42:05 +0330

    Adding UserDto

---
+++ src/code/UsersManagement.WebAPI/Dtos/UserDto.cs
@@ -0,0 +1,9 @@
+﻿namespace UsersManagement.WebAPI.Dtos
+{
+    public class UserDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime Birthday { get; set; }
+    }
+}
